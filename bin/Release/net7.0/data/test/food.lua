c26293c1b969cb72c0b1d1d648cb534fa94708f5
iCryingCat <xzjH5263@163.com>
2022-12-31 04:13:56 +0800

    publish

--- bin/Release/net7.0/data/test/food.lua
+++
@@ -1,9 +0,0 @@
---[[ E:/code/c#/xlsx-exporter/bin/Release/net7.0/assets/测试@test.xlsx ]]--
--- 测试lua枚举
-local XLSX_TEST_FOOD = {
--- 中文
-ZN = 1,
--- 英语
-EN = 3,
-}
-return XLSX_TEST_FOOD
--- bin/Release/net7.0/data/test/user.lua
+++
@@ -1,9 +0,0 @@
---[[ E:/code/c#/xlsx-exporter/bin/Release/net7.0/assets/测试@test.xlsx ]]--
--- 测试表
-local XLSX_TEST_USER = {
--- 昵称
-name = '白泽',
--- 年龄
-age = 20,
-}
-return XLSX_TEST_USER
